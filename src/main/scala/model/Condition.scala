506b16ef7e269569d9efbf4d228b6c1c3ee32ccd
LLCampos <luis.filipe.lcampos@gmail.com>
2021-03-08 17:22:54 +0000

    implement areConditionsCompatible

--- src/main/scala/model/Condition.scala
+++ src/main/scala/model/Condition.scala
@@ -3,7 +3,8 @@ package model
 sealed trait Condition
 
 object Condition {
-  def areConditionsCompatible(candidateCondition: CandidateCondition, employersCondition: EmployersCondition): Boolean = ???
+  def areConditionsCompatible(candidateCondition: CandidateCondition, employersCondition: EmployersCondition): Boolean =
+    candidateCondition.minSalaryAcceptable <= employersCondition.maxSalaryAcceptable
 }
 
 case class CandidateCondition(minSalaryAcceptable: Int) extends Condition
