042b20c2e1a7dc1d54354974985f37f5ffad129c
LLCampos <luis.filipe.lcampos@gmail.com>
2021-03-09 17:37:16 +0000

    add it test

--- src/it/scala/SalaryStandoffApiSpec.scala
+++ src/it/scala/SalaryStandoffApiSpec.scala
@@ -58,108 +58,27 @@ class SalaryStandoffApiSpec extends AnyWordSpec with Matchers with BeforeAndAfte
       employerResponse.areConditionsCompatible shouldBe true
     }
 
-    "if conditions compatible, POST /employer_condition returns true" in pending
+    "if conditions compatible, POST /employer_condition returns true" in {
+      val candidateCondition = CandidateCondition(minSalaryAcceptable = 50)
+      val employerCondition = EmployersCondition(maxSalaryAcceptable = 40)
+
+      val candidateRequest = Request[IO](
+        method = Method.POST,
+        uri = Uri.unsafeFromString(s"$urlStart/candidate_condition")
+      ).withEntity(candidateCondition.asJson)
+
+      val candidateResponse = client.use(_.expect[Json](candidateRequest)).unsafeRunSync().as[PostCandidateConditionResponse].toOption.get
+
+      val employerRequest = Request[IO](
+        method = Method.POST,
+        uri = Uri.unsafeFromString(s"$urlStart/employer_condition/${candidateResponse.conditionId}")
+      ).withEntity(employerCondition.asJson)
+
+      val employerResponse = client.use(_.expect[Json](employerRequest)).unsafeRunSync().as[PostEmployerConditionResponse].toOption.get
+
+      employerResponse.areConditionsCompatible shouldBe false
+    }
+
     "can't check one condition for compatibility more than once" in pending
-
-//    "create a todo" in {
-//      val description = "my todo 1"
-//      val importance = "high"
-//      val createJson =json"""
-//        {
-//          "description": $description,
-//          "importance": $importance
-//        }"""
-//      val request = Request[IO](method = Method.POST, uri = Uri.unsafeFromString(s"$urlStart/todos")).withEntity(createJson)
-//      val json = client.use(_.expect[Json](request)).unsafeRunSync()
-//      root.id.long.getOption(json).nonEmpty shouldBe true
-//      root.description.string.getOption(json) shouldBe Some(description)
-//      root.importance.string.getOption(json) shouldBe Some(importance)
-//    }
-//
-//    "update a todo" in {
-//      val id = createTodo("my todo 2", "low")
-//
-//      val description = "updated todo"
-//      val importance = "medium"
-//      val updateJson = json"""
-//        {
-//          "description": $description,
-//          "importance": $importance
-//        }"""
-//      val request = Request[IO](method = Method.PUT, uri = Uri.unsafeFromString(s"$urlStart/todos/$id")).withEntity(updateJson)
-//      client.use(_.expect[Json](request)).unsafeRunSync() shouldBe json"""
-//        {
-//          "id": $id,
-//          "description": $description,
-//          "importance": $importance
-//        }"""
-//    }
-//
-//    "return a single todo" in {
-//      val description = "my todo 3"
-//      val importance = "medium"
-//      val id = createTodo(description, importance)
-//      client.use(_.expect[Json](Uri.unsafeFromString(s"$urlStart/todos/$id"))).unsafeRunSync() shouldBe json"""
-//        {
-//          "id": $id,
-//          "description": $description,
-//          "importance": $importance
-//        }"""
-//    }
-//
-//    "delete a todo" in {
-//      val description = "my todo 4"
-//      val importance = "low"
-//      val id = createTodo(description, importance)
-//      val deleteRequest = Request[IO](method = Method.DELETE, uri = Uri.unsafeFromString(s"$urlStart/todos/$id"))
-//      client.use(_.status(deleteRequest)).unsafeRunSync() shouldBe Status.NoContent
-//
-//      val getRequest = Request[IO](method = Method.GET, uri = Uri.unsafeFromString(s"$urlStart/todos/$id"))
-//      client.use(_.status(getRequest)).unsafeRunSync() shouldBe Status.NotFound
-//    }
-//
-//    "return all todos" in {
-//      // Remove all existing todos
-//      val json = client.use(_.expect[Json](Uri.unsafeFromString(s"$urlStart/todos"))).unsafeRunSync()
-//      root.each.id.long.getAll(json).foreach { id =>
-//        val deleteRequest = Request[IO](method = Method.DELETE, uri = Uri.unsafeFromString(s"$urlStart/todos/$id"))
-//        client.use(_.status(deleteRequest)).unsafeRunSync() shouldBe Status.NoContent
-//      }
-//
-//      // Add new todos
-//      val description1 = "my todo 1"
-//      val description2 = "my todo 2"
-//      val importance1 = "high"
-//      val importance2 = "low"
-//      val id1 = createTodo(description1, importance1)
-//      val id2 = createTodo(description2, importance2)
-//
-//      // Retrieve todos
-//      client.use(_.expect[Json](Uri.unsafeFromString(s"$urlStart/todos"))).unsafeRunSync() shouldBe json"""
-//        [
-//          {
-//            "id": $id1,
-//            "description": $description1,
-//            "importance": $importance1
-//          },
-//          {
-//            "id": $id2,
-//            "description": $description2,
-//            "importance": $importance2
-//          }
-//        ]"""
-//    }
   }
-
-//  private def createTodo(description: String, importance: String): Long = {
-//    val createJson = json"""
-//      {
-//        "description": $description,
-//        "importance": $importance
-//      }"""
-//    val request = Request[IO](method = Method.POST, uri = Uri.unsafeFromString(s"$urlStart/todos")).withEntity(createJson)
-//    val json = client.use(_.expect[Json](request)).unsafeRunSync()
-//    root.id.long.getOption(json).nonEmpty shouldBe true
-//    root.id.long.getOption(json).get
-//  }
 }
