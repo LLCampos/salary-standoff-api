e31c47f93726586633c500f6d2c98bb5d49a29a6
LLCampos <luis.filipe.lcampos@gmail.com>
2021-03-20 09:09:40 +0000

    use postgres in heroku

--- src/main/scala/db/Database.scala
+++ src/main/scala/db/Database.scala
@@ -1,23 +1,33 @@
 package db
 
 import cats.effect.{Blocker, ContextShift, IO, Resource}
+import com.zaxxer.hikari.HikariConfig
 import config.DatabaseConfig
 import doobie.hikari.HikariTransactor
 import org.flywaydb.core.Flyway
 
 import scala.concurrent.ExecutionContext
+import scala.util.Properties
 
 object Database {
-  def transactor(config: DatabaseConfig, executionContext: ExecutionContext, blocker: Blocker)(implicit contextShift: ContextShift[IO]): Resource[IO, HikariTransactor[IO]] = {
-    HikariTransactor.newHikariTransactor[IO](
-      config.driver,
-      config.url,
-      config.user,
-      config.password,
-      executionContext,
-      blocker
-    )
-  }
+  def transactor(config: DatabaseConfig, executionContext: ExecutionContext, blocker: Blocker)(implicit contextShift: ContextShift[IO]): Resource[IO, HikariTransactor[IO]] =
+    Properties.envOrNone("JDBC_DATABASE_URL") match {
+      // Heroku
+      case Some(dbUrl) =>
+        val hikariConfig = new HikariConfig()
+        hikariConfig.setJdbcUrl(dbUrl)
+        HikariTransactor.fromHikariConfig(hikariConfig, executionContext, blocker)
+      // Local
+      case None =>
+        HikariTransactor.newHikariTransactor[IO](
+          config.driver,
+          config.url,
+          config.user,
+          config.password,
+          executionContext,
+          blocker
+        )
+    }
 
   def initialize(transactor: HikariTransactor[IO]): IO[Unit] = {
     transactor.configure { dataSource =>
