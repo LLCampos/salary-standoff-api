bc16585ce8066a35fad33a3d275a7e741f82e36b
LLCampos <luis.filipe.lcampos@gmail.com>
2021-03-24 17:43:11 +0000

    increase max size of currency

--- src/main/resources/db/migration/V1__create_condition_table.sql
+++ src/main/resources/db/migration/V1__create_condition_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE condition(
     id SERIAL PRIMARY KEY,
     uuid VARCHAR UNIQUE NOT NULL,
     candidate_min_acceptable NUMERIC NOT NULL,
-    currency VARCHAR(8) NOT NULL,
+    currency VARCHAR(16) NOT NULL,
     gross_or_net VARCHAR(8) NOT NULL,
     annual_or_monthly VARCHAR(8) NOT NULL,
     extra_comments VARCHAR(64),
